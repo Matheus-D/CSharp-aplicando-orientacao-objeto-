acf648d9b9c8f12e5827cf4ca0b5d652add01203
Matheus Domingues <59287764+Matheus-D@users.noreply.github.com>
2023-07-19 17:36:14 -0400

    02_ObterSaldo e DefinirSaldo

--- ByteBank/05-ByteBank/Program.cs
+++ ByteBank/05-ByteBank/Program.cs
@@ -23,18 +23,22 @@ namespace _05_ByteBank
             //conta.titular.Cpf = "434.562.878-20";
             //conta.titular.Profissao = "Desenvolvedora C#";
 
+            ContaCorrente ContaDaCamila = new ContaCorrente();
+            ContaDaCamila.titular = new Cliente();
+            ContaDaCamila.titular.Nome = "Camila";
+
             conta.Saldo = 500;
             conta.Agencia = 563;
             conta.Numero = 5634527;
 
-            if(conta.titular == null)
-            {
-                Console.WriteLine("Ops, a refência em conta.titular é NULL");
-            }
+            //if(conta.titular == null)
+            //{
+            //    Console.WriteLine("Ops, a refência em conta.titular é NULL");
+            //}
 
 
             //Console.WriteLine(Gabriela.Nome);
-            Console.WriteLine(conta.titular);
+            Console.WriteLine(ContaDaCamila.titular.Nome);
             //Console.WriteLine(conta.titular.Nome);
             //Console.WriteLine(conta.titular.Cpf);
             //Console.WriteLine(conta.titular.Profissao);
