a3db4882e8c4354274f678a9f4ac218635fa8a94
Matheus Domingues <59287764+Matheus-D@users.noreply.github.com>
2023-07-20 15:02:48 -0400

    Classe Funcionario

---
+++ Bytebank/Bytebank/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bytebank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
